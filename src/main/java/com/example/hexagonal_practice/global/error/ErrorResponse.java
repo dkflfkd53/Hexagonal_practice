8e6ec103c5b2b67b270991d7fd689ff26d15e280
JungHyun <b57062562@gmail.com>
2023-08-08 19:30:41 +0900

    🌧 :: error기본 설정 추가

---
+++ src/main/java/com/example/hexagonal_practice/global/error/ErrorResponse.java
@@ -0,0 +1,42 @@
+package com.example.hexagonal_practice.global.error;
+
+import lombok.*;
+import lombok.extern.slf4j.Slf4j;
+import org.hibernate.exception.ConstraintViolationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.time.LocalDateTime;
+
+@Builder
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class ErrorResponse {
+
+    private String message;
+    private Integer status;
+    private LocalDateTime timestamp;
+    private String description;
+
+    public static ErrorResponse of(ErrorCode errorCode, String description) {
+        return ErrorResponse.builder()
+                .message(errorCode.getMessage())
+                .status(errorCode.getStatusCode())
+                .timestamp(LocalDateTime.now())
+                .description(description)
+                .build();
+    }
+
+    public static ErrorResponse of(int statusCode, String description) {
+        return ErrorResponse.builder()
+                .message(description)
+                .status(statusCode)
+                .timestamp(LocalDateTime.now())
+                .description(description)
+                .build();
+    }
+
+}
