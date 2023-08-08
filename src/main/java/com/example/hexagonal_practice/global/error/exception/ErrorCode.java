58e02918f2c4a9e75bccbf308b1635e1760321a7
JungHyun <b57062562@gmail.com>
2023-08-08 19:33:50 +0900

    🌧 :: errorCode 추가

---
+++ src/main/java/com/example/hexagonal_practice/global/error/exception/ErrorCode.java
@@ -0,0 +1,26 @@
+package com.example.hexagonal_practice.global.error.exception;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+@JsonFormat(shape = JsonFormat.Shape.OBJECT)
+public enum ErrorCode {
+
+    // jwt
+    EXPIRED_TOKEN(401 , "Expired token"),
+    INVALID_TOKEN(401, "Invalid token"),
+    REFRESH_TOKEN_NOT_FOUND(403,"RefreshToken not found"),
+
+    //board
+    BOARD_NOT_FOUND(404, "Board not found"),
+
+    // general
+    BAD_REQUEST(400, "Bad request"),
+    INTERNAL_SERVER_ERROR(500, "Internal server error");
+
+    private final int statusCode;
+    private final String message;
+}
