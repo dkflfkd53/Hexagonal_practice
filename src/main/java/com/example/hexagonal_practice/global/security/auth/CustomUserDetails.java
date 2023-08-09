a4294f698320416fefeb263d7a7dd39437061bff
JungHyun <b57062562@gmail.com>
2023-08-09 11:07:09 +0900

    ♻️ :: customUserDetails 변경

--- src/main/java/com/example/hexagonal_practice/global/security/auth/CustomUserDetails.java
+++ src/main/java/com/example/hexagonal_practice/global/security/auth/CustomUserDetails.java
@@ -1,11 +1,9 @@
 package com.example.hexagonal_practice.global.security.auth;
 
-import com.example.hexagonal_practice.account.domain.User;
+import com.example.hexagonal_practice.domain.user.domain.User;
 import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
-import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
 
@@ -17,13 +15,14 @@ public record CustomUserDetails(User user) implements UserDetails {
     }
 
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return new ArrayList<>(Collections.singleton(new SimpleGrantedAuthority("ROLE_"+user.getType().toString())));
+        return Collections.emptyList();
     }
 
     @Override
     public String getPassword() {
         return null;
     }
+
     @Override
     public boolean isAccountNonExpired() { return true; }
 
