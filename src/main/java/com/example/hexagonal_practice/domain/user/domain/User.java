054d2e362c54022a4afcf59770b3df3339b377ea
JungHyun <b57062562@gmail.com>
2023-08-09 11:06:42 +0900

    ♻️ :: board, user 구조 변경

---
+++ src/main/java/com/example/hexagonal_practice/domain/user/domain/User.java
@@ -0,0 +1,27 @@
+package com.example.hexagonal_practice.domain.user.domain;
+
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String userId;
+
+    private String username;
+
+    private String password;
+
+}
