72f5aee289f32e8cf94d22464400aaf93b663a95
JungHyun <b57062562@gmail.com>
2023-08-09 10:22:39 +0900

    userdetails 추가

---
+++ src/main/java/com/example/hexagonal_practice/global/security/auth/CustomUserDetailsService.java
@@ -0,0 +1,26 @@
+package com.example.hexagonal_practice.global.security.auth;
+
+import com.example.hexagonal_practice.account.adapter.out.persistence.UserRepository;
+import com.example.hexagonal_practice.account.domain.User;
+import com.example.hexagonal_practice.global.exception.user.UserNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String userId) {
+
+        User user = userRepository.findByUserId(userId)
+                .orElseThrow(()-> UserNotFoundException.EXCEPTION);
+
+        return new CustomUserDetails(user);
+    }
+
+}
