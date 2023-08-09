8e05fa9e30f25d56d406b2368b5bf2a95bc6c6f9
JungHyun <b57062562@gmail.com>
2023-08-09 11:39:24 +0900

    ♻️ :: board useCase 묶기

--- src/main/java/com/example/hexagonal_practice/account/application/port/in/board/DeleteBoardUseCase.java
+++ src/main/java/com/example/hexagonal_practice/account/application/port/in/board/DeleteBoardUseCase.java
@@ -1,4 +1,4 @@
-package com.example.hexagonal_practice.account.application.port.in;
+package com.example.hexagonal_practice.account.application.port.in.board;
 
 public interface DeleteBoardUseCase {
     void deleteBoard(Long boardId);
--- src/main/java/com/example/hexagonal_practice/account/application/port/in/board/WriteBoardUseCase.java
+++ src/main/java/com/example/hexagonal_practice/account/application/port/in/board/WriteBoardUseCase.java
@@ -1,4 +1,4 @@
-package com.example.hexagonal_practice.account.application.port.in;
+package com.example.hexagonal_practice.account.application.port.in.board;
 
 import com.example.hexagonal_practice.account.adapter.dto.request.BoardRequest;
 
