953952bf2fd10e16dc6f71b1cafea7232c9d893b
JungHyun <b57062562@gmail.com>
2023-08-10 16:41:43 +0900

    🌧 :: board 작성시 user 외래키 저장

--- src/main/java/com/example/hexagonal_practice/account/application/service/board/WriteBoardService.java
+++ src/main/java/com/example/hexagonal_practice/account/application/service/board/WriteBoardService.java
@@ -3,7 +3,9 @@ package com.example.hexagonal_practice.account.application.service.board;
 import com.example.hexagonal_practice.account.adapter.dto.request.BoardRequest;
 import com.example.hexagonal_practice.account.application.port.in.board.WriteBoardUseCase;
 import com.example.hexagonal_practice.account.application.port.out.BoardRepositoryPort;
+import com.example.hexagonal_practice.account.application.service.user.facade.UserFacade;
 import com.example.hexagonal_practice.account.domain.Board;
+import com.example.hexagonal_practice.account.domain.User;
 import com.example.hexagonal_practice.common.UseCase;
 import lombok.RequiredArgsConstructor;
 import org.springframework.transaction.annotation.Transactional;
@@ -14,11 +16,15 @@ import org.springframework.transaction.annotation.Transactional;
 public class WriteBoardService implements WriteBoardUseCase {
 
     private final BoardRepositoryPort boardRepositoryPort;
+    private final UserFacade userFacade;
 
     public void writeBoard(BoardRequest request) {
+        User user = userFacade.currentUser();
+
         Board board = Board.builder()
                 .title(request.getTitle())
                 .content(request.getContent())
+                .user(user)
                 .build();
 
         boardRepositoryPort.saveBoard(board);
