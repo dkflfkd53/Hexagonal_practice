943856e884e192577438e6b6dd74526345123baa
JungHyun <b57062562@gmail.com>
2023-08-10 16:31:43 +0900

    🌧 :: board user 연관관계 매핑

--- src/main/java/com/example/hexagonal_practice/account/domain/Board.java
+++ src/main/java/com/example/hexagonal_practice/account/domain/Board.java
@@ -2,10 +2,7 @@ package com.example.hexagonal_practice.account.domain;
 
 import lombok.*;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 @Getter
@@ -17,6 +14,9 @@ public class Board {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @ManyToOne
+    private User user;
 
     private String title;
 
