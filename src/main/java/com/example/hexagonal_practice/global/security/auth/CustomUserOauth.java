8bf09f2020905430d4db9e270882d209747e5673
JungHyun <b57062562@gmail.com>
2023-08-10 13:20:15 +0900

    🌧 :: CustomUserDetails분리

---
+++ src/main/java/com/example/hexagonal_practice/global/security/auth/CustomUserOauth.java
@@ -0,0 +1,56 @@
+package com.example.hexagonal_practice.global.security.auth;
+
+import com.example.hexagonal_practice.account.domain.User;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Map;
+
+public record CustomUserOauth(User user, Map<String, Object> attributes) implements UserDetails, OAuth2User {
+
+    @Override
+    public String getName() {
+        return null;
+    }
+
+    @Override
+    public Map<String, Object> getAttributes() {
+        return attributes;
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getUserId();
+    }
+
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.emptyList();
+    }
+
+    @Override
+    public String getPassword() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() { return true; }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+}
