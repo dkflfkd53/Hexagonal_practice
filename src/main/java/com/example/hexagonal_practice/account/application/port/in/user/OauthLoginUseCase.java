20ad90acd2709a4207dabb9f54effd390082e393
JungHyun <b57062562@gmail.com>
2023-08-14 16:34:28 +0900

    🌧 :: oauthLogin UseCase 수정

--- src/main/java/com/example/hexagonal_practice/account/application/port/in/user/OauthLoginUseCase.java
+++ src/main/java/com/example/hexagonal_practice/account/application/port/in/user/OauthLoginUseCase.java
@@ -1,7 +1,5 @@
 package com.example.hexagonal_practice.account.application.port.in.user;
 
-import org.springframework.http.ResponseEntity;
-
 public interface OauthLoginUseCase {
-    ResponseEntity<String> getGoogleAccessToken(String accessCode);
+    void oauthLogin(String code, String registrationId);
 }
